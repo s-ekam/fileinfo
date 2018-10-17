e8d2ecc07e75dd964d8d1dccc53d23d365bc3a2f
s-ekam97 <s.ekam97@gmail.com>
2018-10-18 01:23:38 +0530

    inital Testing

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-x = requests.get("https://fileinfo.com/extension/cpp")
+x = requests.get("https://fileinfo.com/extension/pdf")
 # print(x.content)
 
 soup = bs(x.content,'html.parser')
-# pret = soup.prettify()
+pret = soup.prettify()
 # ls = list(soup.children)
 # ls = str(ls)
 # # print(ls)
@@ -14,4 +14,16 @@ soup = bs(x.content,'html.parser')
 # <p><span itemprop="description">
 z = soup.find_all('p')
 st = str(z[1])
+# print(pret)
+z = soup.find_all('a')
+# c =0
+# for x in z:
+# 	print(c,end="  ")
+# 	print(x)
+# 	c = c+1
+# print(z)
+
+st2 = str(z[10])
+
+print(st2[33:-4])
 print(st[32:-12])
